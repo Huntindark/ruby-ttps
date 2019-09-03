2022e87b62307c384d1f2137d3afa4e06fee22ee
Federico <fedeferf1@gmail.com>
2019-09-03 11:27:06 -0300

    added first problem resolution

---
+++ res_practicas/practica_uno/ej_1.rb
@@ -0,0 +1,14 @@
+# Investigá y probá en un intérprete de Ruby cómo crear objetos de los siguientes tipos
+# básicos usando literales y usando el constructor new (cuando sea posible):
+# Arreglo (Array)
+# Diccionario o hash (Hash)
+# String (String)
+# Símbolo (Symbol)
+
+array = Array.new
+
+dic = Hash.new
+
+string = String.new
+
+symbol = :text
